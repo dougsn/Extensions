0f70b330c0bc6dfa0e7ae0db6cd96c7927557b6d
Douglas da Silva Nascimento <dnascimento@enavirj.com.br>
2023-02-01 14:12:50 -0300

    Finalizando filtro de nome e setor

--- src/Ramal.App/wwwroot/js/site.js
+++ src/Ramal.App/wwwroot/js/site.js
@@ -1,4 +1,17 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(function () {
+    $(".tabela input").keyup(function () {
+        var index = $(this).parent().index();
+        var nth = ".tabela td:nth-child(" + (index + 1).toString() + ")";
+        var valor = $(this).val().toUpperCase();
+        $(".tabela tbody tr").show();
+        $(nth).each(function () {
+            if ($(this).text().toUpperCase().indexOf(valor) < 0) {
+                $(this).parent().hide();
+            }
+        });
+    });
 
-// Write your JavaScript code.
+    $(".tabela input").blur(function () {
+        $(this).val("");
+    });
+});
