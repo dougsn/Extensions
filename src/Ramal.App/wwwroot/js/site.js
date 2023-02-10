17db9bb73a9db148f607d6f749b6fe53abb76e3e
Douglas da Silva Nascimento <dnascimento@enavirj.com.br>
2023-02-10 14:08:51 -0300

    Finalizando filtro por select nos ramais

--- src/Ramal.App/wwwroot/js/site.js
+++ src/Ramal.App/wwwroot/js/site.js
@@ -1,4 +1,20 @@
 ﻿$(function () {
+    $(".tabela .filtro").change(function () {
+        var index = $(this).parent().index();
+        var nth = ".tabela td:nth-child(" + (index + 1).toString() + ")";
+        var valor = $(this).val().toUpperCase();
+        $(".tabela tbody tr").show();
+        $(nth).each(function () {
+            if ($(this).text().toUpperCase().indexOf(valor) < 0) {
+                $(this).parent().hide();
+            }
+        });
+    });
+
+    $(".tabela .filtro").blur(function () {
+        $(this).val("");
+    });
+
     $(".tabela input").keyup(function () {
         var index = $(this).parent().index();
         var nth = ".tabela td:nth-child(" + (index + 1).toString() + ")";
